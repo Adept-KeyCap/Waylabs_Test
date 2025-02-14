88238a67b26359a651144c85889dc3758eb158e4
Adept-KeyCap <82065985+Adept-KeyCap@users.noreply.github.com>
2025-02-13 22:56:42 -0500

    base layout for Enemy State Machine

--- TechnicalTest/Assets/1-Scripts/Enemy/EnemyStateMachine.cs
+++ TechnicalTest/Assets/1-Scripts/Enemy/EnemyStateMachine.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngineInternal;
 
 public class EnemyStateMachine : MonoBehaviour
 {
-    [SerializeField] private EnemyState enemyState;
+    public Coroutine currentBehaviour;
+
     // float: Speed || bool: Fallen || Triggers: Fall, Die, Hit
+    [SerializeField] private EnemyState enemyState;
+    [SerializeField] private NavMeshAgent agent;
+
     [SerializeField] private Animator animator; 
-    [SerializeField] private GameObject target;
+    [SerializeField] private Transform target;
+
+    
+
+    private float movementSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
+        agent = GetComponent<NavMeshAgent>();
+
         if (animator == null)
         {
             animator = GetComponentInChildren<Animator>();
@@ -27,7 +38,61 @@ public class EnemyStateMachine : MonoBehaviour
 
     private void UpateState(EnemyState state)
     {
+        if (currentBehaviour != null)
+        {
+            StopCoroutine(currentBehaviour);
+        }
+        switch(state)
+        {
+            case EnemyState.Idle:
+                currentBehaviour = StartCoroutine(Idle());
+                break;
 
+            case EnemyState.Chase:
+                currentBehaviour = StartCoroutine(Chase());
+                break;
+
+            case EnemyState.Crawl:
+                currentBehaviour = StartCoroutine(Crawl());
+                break;
+
+            case EnemyState.Dead:
+                currentBehaviour = StartCoroutine(Dead());
+
+                break;
+        }
+    }
+
+    private IEnumerator Idle()
+    {
+        yield return null;
+
+    }
+
+    private IEnumerator Chase()
+    {
+        while (true)
+        {
+            agent.destination = target.position;
+
+            yield return null;
+        }
+    }
+
+    private IEnumerator Crawl()
+    {
+        yield return null;
+
+    }
+
+    private IEnumerator Dead()
+    {
+        yield return null;
+    }
+
+    private IEnumerator FallingAnimationTimming()
+    {
+        yield return null;
     }
 
     public void LegsBusted()
